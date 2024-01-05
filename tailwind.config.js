b5d602d5db49c7903de402fa1df0b22ef10e61a1
unknown <ibragans666@gmail.com>
2024-01-05 20:20:51 +0700

    portfolio saya

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding : '16px',
+    },
+    fontFamily : {
+      sans: [ "Nunito", "sans-serif" ],
+
+    },
+    extend: {
+      colors: {
+        primary : "#c12525",
+        secondary : '#64748b',
+        dark : '#2D3250',
+        light : '#F9FAFB',
+        portosec : '#323E48',
+        ijo : '#2BC377',
+        oren : '#957723',
+        ijotua : '#0F5E24'
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+
+    },
+  },
+  plugins: [],
+}
+
